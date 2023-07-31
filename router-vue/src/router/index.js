ae02ce35bb6bdf43320da1b0a06953429cb917dd
azizf098 <21221753104@smkassalaambandung.sch.id>
2023-07-31 15:14:44 +0700

    belajar router-vue

---
+++ router-vue/src/router/index.js
@@ -0,0 +1,44 @@
+import { createWebHistory, createRouter } from "vue-router";
+// Import Component2 Kalian
+
+import Home from "@/components/Home.vue"; 
+import About from "@/components/About.vue";
+import Contact from "@/components/Contact.vue";
+import Produk from "@/components/Produk.vue";
+import Detail from "@/components/Detail.vue";
+
+const routes = [ 
+  {
+    path: "/", 
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: Contact,
+  },
+  {
+    path: "/produk",
+    name: "Produk",
+    component: Produk,
+  },
+  {
+    path: "/detail/id_produk",
+    name: "Detail",
+    component: Detail,
+    props: true,
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
